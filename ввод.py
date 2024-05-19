cf72c78c94911c07bf05236f9ab65d57b1f5acc3
NikEr2024 <n.erdyneev2000@gmail.com>
2024-05-19 21:24:07 +0300

    Add files via upload

---
+++ ввод.py
@@ -0,0 +1,30 @@
+print('Задача 1 ') #ввывод на консоль
+print('Habib Nurmagomedov'[6])
+print('Habib Nurmagomedov' [:7])
+
+print()
+print('Задача 2')# Длина слова
+print(len('str(Habib Nurmagomedov)'))
+print(len('Habib' 'Nurmagomedov'))
+
+print()
+print('Задача 3') # Суммы и разности
+print(23+17)
+print(type(str('23+17')))
+print(23-17)
+print(type(str(23-17)))
+
+print()
+print('Задача 4') # Среднее Арифметическое
+print((2+4+6)/3)
+
+print()
+print('Зачача 5') # Простые строчки
+print()
+a = 'Понедельник'
+b = 'Вторник'
+print('Понедельник, ' 'Вторник')
+
+print()
+print('Задача 6') #Сложная формyлa
+input('(((a*b)+(a*c)**3)/2')
